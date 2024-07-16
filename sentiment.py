c4d1cb846231181b8d19dc5c8525830e880806d3
Ronak-edision <130649974+Ronak-edision@users.noreply.github.com>
2024-07-16 22:40:13 +0545

    Update5 sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -3,6 +3,7 @@ from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 import requests
+from post_install import *
 
 
 
