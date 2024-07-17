b6968cbe8e0a8b9fb1b113bdce95857be7c7574c
Ronak-edision <130649974+Ronak-edision@users.noreply.github.com>
2024-07-17 10:50:55 +0545

    Update post_install.py

--- post_install.py
+++ post_install.py
@@ -1,13 +1,7 @@
-# post_install.py
 import nltk
-import ssl
 
-def download_nltk_data():
-    try:
-        nltk.data.find("corpora/stopwords")
-    except (LookupError, ssl.SSLError):
-        ssl._create_default_https_context = ssl._create_unverified_context
-        nltk.download('stopwords', download_dir='nltk_data', quiet=True)
+def setup_nltk_data():
+    nltk.data.path.append('./nltk_data')  # Adjust path as necessary
 
 if __name__ == "__main__":
-    download_nltk_data()
+    setup_nltk_data()
