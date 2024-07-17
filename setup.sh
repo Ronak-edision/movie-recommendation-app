98fff49b1ea07830e5be45ff92e746488e5cbe52
Ronak-edision <130649974+Ronak-edision@users.noreply.github.com>
2024-07-17 08:16:19 +0545

    Update6 setup.sh

--- setup.sh
+++ setup.sh
@@ -7,9 +7,3 @@ headless= true\n\
 \n\
 ">~/.streamlit/config.toml
 
-# Download NLTK stopwords to a custom directory
-mkdir -p ~/.nltk_data
-python -m nltk.downloader -d ~/.nltk_data stopwords
-
-# Set NLTK_DATA environment variable
-export NLTK_DATA=~/.nltk_data
