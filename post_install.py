24ceec97ee2fb93ebe4c063d4c3bda211aeea6b3
Ronak-edision <130649974+Ronak-edision@users.noreply.github.com>
2024-07-17 09:38:36 +0545

    Update post_install.py

--- post_install.py
+++ post_install.py
@@ -1,9 +1,14 @@
 # post_install.py
 import nltk
+from nltk.data import find
+import ssl
 
 def download_nltk_data():
-    nltk.download('stopwords')
+    try:
+        find("corpora/stopwords.zip")
+    except LookupError:
+        context = ssl._create_unverified_context()
+        nltk.download('stopwords', download_dir='nltk_data', quiet=True, context=context)
 
 if __name__ == "__main__":
-    nltk.download('punkt')  # Ensure necessary NLTK data is downloaded
     download_nltk_data()
