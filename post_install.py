228b45173c58da0cfce6bc3a01605c79d39117f6
Ronak-edision <130649974+Ronak-edision@users.noreply.github.com>
2024-07-17 09:36:58 +0545

    Update post_install.py

--- post_install.py
+++ post_install.py
@@ -1,7 +1,9 @@
+# post_install.py
 import nltk
 
 def download_nltk_data():
     nltk.download('stopwords')
 
 if __name__ == "__main__":
+    nltk.download('punkt')  # Ensure necessary NLTK data is downloaded
     download_nltk_data()
